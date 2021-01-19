521b24387eee5106bd4356df5fa88ad09ce08636
Nikolay Chechulin <nchechulin@aol.com>
2021-01-19 17:11:00 +0300

    add handler for delete_record request

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
+from json import dumps
 from flask import Flask, request
+from db import Database
 
 app = Flask(__name__)
+db = Database("documents.sqlite")
 
 
 @app.route("/delete_record", methods=["GET"])
 def delete_record_handler():
-    return "not ready yet..."
+    try:
+        id = int(request.args["id"])
+        db.try_delete_post_by_id(id)
+        return "Success"
+    except Exception as exc:
+        return str(exc)
 
 
-@app.route("/get_records", methods=["GET"])
-def get_records_handler():
+@app.route("/get_posts", methods=["GET"])
+def get_posts_handler():
     return "not ready yet..."
 
 
